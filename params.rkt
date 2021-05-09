4e48004af8ba382ef775e8598c5023f7d00cc5b0
louis-fraysr <louis.frayser@gmail.com>
2021-05-08 17:06:21 -0700

    removed obsolete vboxpci from essential modules modules

--- params.rkt
+++ params.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (provide %kname% %modules% %modnames%)
 (define %kname% "ghost")
-(define %modules% '("loop" "zfs" "spl" "vboxpci" "vboxnetadp"
+(define %modules% '("loop" "zfs" "spl"  "vboxnetadp"
                             "vboxnetflt" "vboxdrv" ))
 (define %modnames% (map (lambda(s)(string-append s ".ko")) %modules%))
